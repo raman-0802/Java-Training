5f14dc0d95f1ee7383f2696ed9b8fd7b05eee609
raman-0802 <mishraramanjee0@gmail.com>
2025-11-03 12:58:56 +0530

    datatype

---
+++ Lecture 1/datatype.java
@@ -0,0 +1,12 @@
+public class datatype{
+    public static void main(String[] args) {
+        byte a=5;// 1byte
+        short b=5;// 2 byte
+        int c=5;// 4 byte
+        float f=90.3f;
+        boolean data=false;
+        System.out.println(b);
+    }
+
+    
+}
