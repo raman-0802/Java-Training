0a8854028f1c6157ad8db575b7172b167c5c4d88
raman-0802 <mishraramanjee0@gmail.com>
2025-11-03 13:16:01 +0530

    operaor

---
+++ operators.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class operators {
+   public operators() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 5;
+      if (var1 % 2 == 0) {
+         System.out.println("even");
+      } else {
+         System.out.println("odd");
+      }
+
+   }
+}
